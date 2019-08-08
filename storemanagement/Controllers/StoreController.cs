b12f5081cae08ac2023040a09cd5ec0f1e803f5e
kivelz <kivelz@hotmail.com>
2019-08-08 16:18:10 +0800

    add logic to display all request and items in view

--- storemanagement/Controllers/StoreController.cs
+++ storemanagement/Controllers/StoreController.cs
@@ -5,17 +5,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using storemanagement.DAL;
+using storemanagement.Models.DTO;
 
 namespace storemanagement.Controllers
 {
     public class StoreController : Controller
     {
-        private RequestDetailsContext _dbItem = new RequestDetailsContext();
-        private RequestDal _request = new RequestDal();
+        private readonly RequestDetailsContext _dbItem = new RequestDetailsContext();
+        private readonly RequestDal _request = new RequestDal();
         // GET: Store
         public ActionResult Index()
         {
+            ListOfRequest request = new ListOfRequest();
 
+            request.item = _dbItem.GetAllRequestItem();
+            request.request = _request.GetAllRequest();
             return View();
         }
     }
