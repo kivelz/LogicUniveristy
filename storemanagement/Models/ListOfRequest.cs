b0036a18c59e484d8a52779891efad252ee7b5fd
kivelz <kivelz@hotmail.com>
2019-08-08 16:14:48 +0800

    add a class of request list for model display

---
+++ storemanagement/Models/ListOfRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace storemanagement.Models.DTO
+{
+    public class ListOfRequest
+    {
+        public List<Request> request { get; set; }
+        public List<RequestItem> item { get; set; }
+    }
+}
