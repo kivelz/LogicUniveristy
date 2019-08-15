02dfcc3f5a7c9b56480038ba48b9c8c2ae54f73b
kivelz <kivelz@hotmail.com>
2019-08-15 16:29:54 +0800

    installed react

---
+++ storemanagement/Scripts/react/webpack.config.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+    mode: 'development',
+    context: __dirname,
+        entry: "./app.js",
+        output: {
+        path: __dirname + "/dist",
+            filename: "bundle.js"
+    },
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_module)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['babel-preset-env', 'babel-preset-react']
+                }
+            }
+        }]
+    }
+
+}
